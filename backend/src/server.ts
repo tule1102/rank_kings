3542039c15b18dfc52972d434dffd68dd832c57a
tule1102 <tule1102@knights.ucf.edu>
2023-05-25 19:20:49 -0400

    changed scripts for deployment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,17 @@
 import app from "./app"
 import "dotenv/config";
 import mongoose from "mongoose";
-// import express from "express";
+import express, { Request, Response } from 'express';
 import env from "./util/validateEnv"
+import path from "path";
 
 const port = env.PORT;
+
+app.use(express.static(path.join(__dirname, '../../frontend/dist')));
+
+app.get('*', (_req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
+});
 
 mongoose.connect(env.MONGO_CONNECTION_STRING)
     .then(() => {
