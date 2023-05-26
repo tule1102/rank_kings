3e6a731654be0c69ffffe4c37ec83cd2e3c4a424
tule1102 <tule1102@gmail.com>
2023-05-26 12:07:21 -0400

    Delete backend/dist directory

--- backend/dist/util/assertIsDefined.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.assertIsDefined = void 0;
-function assertIsDefined(val) {
-    if (!val) {
-        throw Error("Expected 'val' to be defined, but received " + val);
-    }
-}
-exports.assertIsDefined = assertIsDefined;
